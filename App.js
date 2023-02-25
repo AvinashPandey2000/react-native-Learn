c5ec10613f259a16c6a9858fdd898f78233cf614
AvinashPandey2000 <avinash201pandey@gmail.com>
2023-02-25 16:09:32 +0530

    Noting

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
+var name ="avinash"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize:80}}>ram  </Text>
+      <Text style={{fontSize:40}}> Sham</Text>
+      <Button title='click'></Button>
+      <Button title='="click2' ></Button>
+      <Text>{name}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +21,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'  },
 });
